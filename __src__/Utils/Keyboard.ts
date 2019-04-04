e1c5e2a892998100151f2a7cdbb08927dc39e4c0
Enteleform <enteleform@gmail.com>
2019-04-04 19:20:18 -0400

    ..

--- __src__/Utils/Keyboard.ts
+++ __src__/Utils/Keyboard.ts
@@ -3,7 +3,7 @@ import hotkeys from "hotkeys-js"
 
 
 export class Keyboard{
-	bind(keys:string|string[]){
+	static bind(keys:string|string[]){
 		const keysString =
 			(keys.constructor === Array)
 			? (keys as Array<string>).join(", ")
