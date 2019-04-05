f0a676659ad9e4c794dd032d7d6ab5aee16f9f7d
Enteleform <enteleform@gmail.com>
2019-04-05 17:05:48 -0400

    ..

--- __src__/Utils/get_FileText.ts
+++ __src__/Utils/get_FileText.ts
@@ -1,11 +1,30 @@
 // modified from https://stackoverflow.com/a/14446538/4955183
 
+//###############//
+//###  Setup  ###//
+//###############//
 
 const _baseURL = "https://enteleform-extensions.github.io/KanbanTool"
 
 
+//#################//
+//###  Exports  ###//
+//#################//
+
 export async function get_FileText(url:string){
+	const result = {value: null}
+	await _get_FileText(url, result)
+	return result.value
+}
+
+
+//#################//
+//###  Utils  ###//
+//#################//
+
+async function _get_FileText(url:string, result:{value}){
 	const fileURL = url.replace(/^\.\//, `${_baseURL}/`)
+
 	var file = new XMLHttpRequest()
 	file.open("GET", fileURL, false)
 
@@ -16,7 +35,7 @@ export async function get_FileText(url:string){
 		)
 
 		if(fileAcquired)
-			{return file.responseText}
+			{result.value = file.responseText}
 		else{
 			throw new Error(`
 				Unable to resolve URL:
