73aa96a119b3117cbe921749f435bd8375fc0b38
Enteleform <enteleform@gmail.com>
2019-04-06 14:51:59 -0400

    ..

---
+++ .config/webpack/__main__.ts
@@ -0,0 +1,25 @@
+//###  Module  ###//
+import {Settings} from "../Settings"
+
+//###  NPM  ###//
+import webpack from "webpack"
+
+
+export default (env:any): webpack.Configuration => ({
+
+	target:    "web",
+	devtool:   "inline-source-map",
+	devServer: {contentBase:Settings.distributionFolder},
+
+	output: {
+		filename: "__Main__.js",
+		path:     Settings.distributionPath,
+	},
+
+	entry:        require("./entry"       ).default,
+	module:       require("./module"      ).default,
+	optimization: require("./optimization").default,
+	plugins:      require("./plugins"     ).default,
+	resolve:      require("./resolve"     ).default,
+
+})
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,25 +1,2 @@
-//###  Module  ###//
-import {Settings} from "./.config/Settings"
 
-//###  NPM  ###//
-import webpack from "webpack"
-
-
-export default (env:any): webpack.Configuration => ({
-
-	target:    "web",
-	devtool:   "inline-source-map",
-	devServer: {contentBase:Settings.distributionFolder},
-
-	output: {
-		filename: "__Main__.js",
-		path:     Settings.distributionPath,
-	},
-
-	entry:        require("./.config/webpack/entry"       ).default,
-	module:       require("./.config/webpack/module"      ).default,
-	optimization: require("./.config/webpack/optimization").default,
-	plugins:      require("./.config/webpack/plugins"     ).default,
-	resolve:      require("./.config/webpack/resolve"     ).default,
-
-})
+export default require("./.config/webpack/__main__").default
