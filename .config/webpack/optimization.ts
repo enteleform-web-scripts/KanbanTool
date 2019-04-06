56b2b4177e2821b9c6c70d46130416cc8f15064e
Enteleform <enteleform@gmail.com>
2019-04-06 12:09:38 -0400

    ..

--- .config/webpack/optimization.ts
+++ .config/webpack/optimization.ts
@@ -2,28 +2,34 @@
 import {CSS_Splitter} from "./Utils/CSS_Splitter/__main__"
 
 
-//###############//
-//###  Setup  ###//
-//###############//
-
-const cacheGroups = {vendors: false}
-
-CSS_Splitter.fileRegExes.forEach((pattern, i) => {
-	cacheGroups[`Group${i}`] = {
-		name:    CSS_Splitter.get_ChunkName,
-		test:    /CSS\.styl$/,
-		chunks:  "all",
-		enforce: true,
-	}
-})
-
-
 //#################//
 //###  Exports  ###//
 //#################//
 
 export const optimization:any = {
 
-	splitChunks: {cacheGroups},
+	splitChunks: {
+		cacheGroups: _build_CacheGroups_From_RegExes(),
+	},
 
+}
+
+
+//###############//
+//###  Utils  ###//
+//###############//
+
+function _build_CacheGroups_From_RegExes(){
+	const cacheGroups = {}
+
+	CSS_Splitter.fileRegExes.forEach((pattern, i) => {
+		cacheGroups[`Group${i}`] = {
+			name:    CSS_Splitter.get_ChunkName,
+			test:    /CSS\.styl$/,
+			chunks:  "all",
+			enforce: true,
+		}
+	})
+
+	return cacheGroups
 }
