91155fb42ebd2c2f4aa4626d96efe1167a9b4e34
Enteleform <enteleform@gmail.com>
2019-04-06 22:48:21 -0400

    ..

--- __src__/__main__/__main__.ts
+++ __src__/__main__/__main__.ts
@@ -1,8 +1,8 @@
 
 //###  Extensions  ###
-// import "~/Extensions/CardLegend.ts/TEMP"
-// import "~/Extensions/CheckList/TEMP"
-import "~/Extensions/FunctionBar/TEMP"
+import "~/Extensions/CheckList/TEMP"
+import "~/Extensions/FunctionBar/TEMP_ModeLegend"
+import "~/Extensions/FunctionBar/TEMP_CardLegend"
 // import {FunctionBar} from "~/Extensions/FunctionBar/__main__"
 // FunctionBar.load({
 // 	CardLegend: true,
