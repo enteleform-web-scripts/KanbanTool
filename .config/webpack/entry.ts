b6990621880c974a459b9575183517f85a4f19ad
Enteleform <enteleform@gmail.com>
2019-04-06 20:58:36 -0400

    ..

--- .config/webpack/entry.ts
+++ .config/webpack/entry.ts
@@ -5,9 +5,6 @@ import {CSS_Splitter} from "./Utils/CSS_Splitter"
 //###  NPM  ###//
 import webpack from "webpack"
 
-console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-console.log(CSS_Splitter.css_EntryPoints)
-console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 
 const $: (string | string[] | webpack.Entry | webpack.EntryFunc) = {
 	"__Main__": Settings.mainScript,
