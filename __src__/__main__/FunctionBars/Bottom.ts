1afed487d2a6c210bc7abc2fa38fcbeeecc77e76
Enteleform <enteleform@gmail.com>
2019-04-13 02:16:25 -0400

    ..

--- __src__/__main__/FunctionBars/Bottom.ts
+++ __src__/__main__/FunctionBars/Bottom.ts
@@ -12,81 +12,16 @@ export const bottom_FunctionBar = new FunctionBar({
 
 	entryGroups:[
 
-		[
-			new Entry({
-				name: "Active",
-				on_Load: () => {
-					Show.rows({include:["Routine", "Tasks.Active"]})
-					Show.allColumns()
-				},
-			}),
-			new Entry({
-				name: "Plan",
-				on_Load: () => {
-					Show.allColumns()
-					Show.allRows()
-				},
-			}),
-		],
-
-		[
-			new Entry({
-				name: "Routine",
-				on_Load: () => {
-					Show.rows({include:["Routine"]})
-					Show.allColumns()
-				},
-			}),
-			new Entry({
-				name: "Today",
-				on_Load: () => {
-					Show.rows({include:["Tasks.Active"]})
-					Show.allColumns()
-				},
-			}),
-			new Entry({
-				name: "Routine.Short",
-				on_Load: () => {
-					Show.rows({include:["Routine.Short"]})
-					Show.allColumns()
-				},
-			}),
-		],
-
 		// [
 		// 	new Entry({
-		// 		name: "Fitness Math",
+		// 		name: "Active",
 		// 		on_Load: () => {
-		// 			Show.allRows()
-		// 			Show.columns({include:["Fitness", "Math"]})
+		// 			Show.rows({include:["Routine", "Tasks.Active"]})
+		// 			Show.allColumns()
 		// 		},
 		// 	}),
 		// 	new Entry({
-		// 		name: "!(Fitness Math)",
-		// 		on_Load: () => {
-		// 			Show.allRows()
-		// 			Show.columns({exclude:["Fitness", "Math"]})
-		// 		},
-		// 	}),
-		// 	new Entry({
-		// 		name: "Music\\Theory",
-		// 		on_Load: () => {
-		// 			Show.allRows()
-		// 			Show.columns({include:["Music\\Theory"]})
-		// 		},
-		// 	}),
-		// ],
-
-		// [
-		// 	new Entry({
-		// 		name: "HideAll",
-		// 		on_Load: () => {
-		// 			Show.noColumns()
-		// 			Show.noRows()
-		// 		},
-		// 	}),
-		// 	new Entry({
-		// 		name: "ShowAll",
+		// 		name: "Plan",
 		// 		on_Load: () => {
 		// 			Show.allColumns()
 		// 			Show.allRows()
@@ -96,113 +31,178 @@ export const bottom_FunctionBar = new FunctionBar({
 
 		// [
 		// 	new Entry({
-		// 		name: "*",
+		// 		name: "Routine",
 		// 		on_Load: () => {
-		// 			Show.allRows()
-		// 			Show.columns({include:["*"]})
+		// 			Show.rows({include:["Routine"]})
+		// 			Show.allColumns()
 		// 		},
 		// 	}),
 		// 	new Entry({
-		// 		name: "**",
+		// 		name: "Today",
 		// 		on_Load: () => {
-		// 			Show.allRows()
-		// 			Show.columns({include:["**"]})
+		// 			Show.rows({include:["Tasks.Active"]})
+		// 			Show.allColumns()
 		// 		},
 		// 	}),
 		// 	new Entry({
-		// 		name: "**\\*",
+		// 		name: "Routine.Short",
 		// 		on_Load: () => {
-		// 			Show.allRows()
-		// 			Show.columns({include:["**\\*"]})
+		// 			Show.rows({include:["Routine.Short"]})
+		// 			Show.allColumns()
 		// 		},
 		// 	}),
 		// ],
 
-		// [
-		// 	new Entry({
-		// 		name: "B\\*",
-		// 		on_Load: () => {
-		// 			Show.allRows()
-		// 			Show.columns({include:["B\\*"]})
-		// 		},
-		// 	}),
-		// 	new Entry({
-		// 		name: "B\\**",
-		// 		on_Load: () => {
-		// 			Show.allRows()
-		// 			Show.columns({include:["B\\**"]})
-		// 		},
-		// 	}),
-		// 	new Entry({
-		// 		name: "B\\**\\*",
-		// 		on_Load: () => {
-		// 			Show.allRows()
-		// 			Show.columns({include:["B\\**\\*"]})
-		// 		},
-		// 	}),
-		// 	new Entry({
-		// 		name: "B\\***",
-		// 		on_Load: () => {
-		// 			Show.allRows()
-		// 			Show.columns({include:["B\\***"]})
-		// 		},
-		// 	}),
-		// ],
+		[
+			new Entry({
+				name: "Fitness Math",
+				on_Load: () => {
+					Show.allRows()
+					Show.columns({include:["Fitness", "Math"]})
+				},
+			}),
+			new Entry({
+				name: "!(Fitness Math)",
+				on_Load: () => {
+					Show.allRows()
+					Show.columns({exclude:["Fitness", "Math"]})
+				},
+			}),
+			new Entry({
+				name: "Music\\Theory",
+				on_Load: () => {
+					Show.allRows()
+					Show.columns({include:["Music\\Theory"]})
+				},
+			}),
+		],
 
-		// [
-		// 	new Entry({
-		// 		name: "2\\*",
-		// 		on_Load: () => {
-		// 			Show.allRows()
-		// 			Show.columns({include:["2\\*"]})
-		// 		},
-		// 	}),
-		// 	new Entry({
-		// 		name: "2\\**",
-		// 		on_Load: () => {
-		// 			Show.allRows()
-		// 			Show.columns({include:["2\\**"]})
-		// 		},
-		// 	}),
-		// 	new Entry({
-		// 		name: "**\\2\\*",
-		// 		on_Load: () => {
-		// 			Show.allRows()
-		// 			Show.columns({include:["**\\2\\*"]})
-		// 		},
-		// 	}),
-		// 	new Entry({
-		// 		name: "**\\2\\**",
-		// 		on_Load: () => {
-		// 			Show.allRows()
-		// 			Show.columns({include:["**\\2\\**"]})
-		// 		},
-		// 	}),
-		// ],
+		[
+			new Entry({
+				name: "HideAll",
+				on_Load: () => {
+					Show.noColumns()
+					Show.noRows()
+				},
+			}),
+			new Entry({
+				name: "ShowAll",
+				on_Load: () => {
+					Show.allColumns()
+					Show.allRows()
+				},
+			}),
+		],
 
-		// [
-		// 	new Entry({
-		// 		name: "!(B\\2)",
-		// 		on_Load: () => {
-		// 			Show.allRows()
-		// 			Show.columns({exclude:["B\\2\\*"]})
-		// 		},
-		// 	}),
-		// 	new Entry({
-		// 		name: "!(B\\2\\*)",
-		// 		on_Load: () => {
-		// 			Show.allRows()
-		// 			Show.columns({exclude:["B\\2\\*"]})
-		// 		},
-		// 	}),
-		// 	new Entry({
-		// 		name: "!(B\\2\\21)",
-		// 		on_Load: () => {
-		// 			Show.allRows()
-		// 			Show.columns({exclude:["B\\2\\21"]})
-		// 		},
-		// 	}),
-		// ],
+		[
+			new Entry({
+				name: "*",
+				on_Load: () => {
+					Show.allRows()
+					Show.columns({include:["*"]})
+				},
+			}),
+			new Entry({
+				name: "**",
+				on_Load: () => {
+					Show.allRows()
+					Show.columns({include:["**"]})
+				},
+			}),
+			new Entry({
+				name: "**\\*",
+				on_Load: () => {
+					Show.allRows()
+					Show.columns({include:["**\\*"]})
+				},
+			}),
+		],
+
+		[
+			new Entry({
+				name: "B\\*",
+				on_Load: () => {
+					Show.allRows()
+					Show.columns({include:["B\\*"]})
+				},
+			}),
+			new Entry({
+				name: "B\\**",
+				on_Load: () => {
+					Show.allRows()
+					Show.columns({include:["B\\**"]})
+				},
+			}),
+			new Entry({
+				name: "B\\**\\*",
+				on_Load: () => {
+					Show.allRows()
+					Show.columns({include:["B\\**\\*"]})
+				},
+			}),
+			new Entry({
+				name: "B\\***",
+				on_Load: () => {
+					Show.allRows()
+					Show.columns({include:["B\\***"]})
+				},
+			}),
+		],
+
+		[
+			new Entry({
+				name: "2\\*",
+				on_Load: () => {
+					Show.allRows()
+					Show.columns({include:["2\\*"]})
+				},
+			}),
+			new Entry({
+				name: "2\\**",
+				on_Load: () => {
+					Show.allRows()
+					Show.columns({include:["2\\**"]})
+				},
+			}),
+			new Entry({
+				name: "**\\2\\*",
+				on_Load: () => {
+					Show.allRows()
+					Show.columns({include:["**\\2\\*"]})
+				},
+			}),
+			new Entry({
+				name: "**\\2\\**",
+				on_Load: () => {
+					Show.allRows()
+					Show.columns({include:["**\\2\\**"]})
+				},
+			}),
+		],
+
+		[
+			new Entry({
+				name: "!(B\\2)",
+				on_Load: () => {
+					Show.allRows()
+					Show.columns({exclude:["B\\2\\*"]})
+				},
+			}),
+			new Entry({
+				name: "!(B\\2\\*)",
+				on_Load: () => {
+					Show.allRows()
+					Show.columns({exclude:["B\\2\\*"]})
+				},
+			}),
+			new Entry({
+				name: "!(B\\2\\21)",
+				on_Load: () => {
+					Show.allRows()
+					Show.columns({exclude:["B\\2\\21"]})
+				},
+			}),
+		],
 
 	],
 
