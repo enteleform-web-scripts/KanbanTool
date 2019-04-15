a6c12a6f072f07c17999a8c7c12fa11a58c2df84
Enteleform <enteleform@gmail.com>
2019-04-15 13:37:41 -0400

    ..

--- __src__/__main__/__main__.ts
+++ __src__/__main__/__main__.ts
@@ -6,10 +6,10 @@ import "~/Extensions/CheckList/TEMP"
 
 import {FunctionBar} from "~/Extensions/FunctionBar/__Main__"
 FunctionBar.load(
-	require("./FunctionBars/Bottom_CardFilters"),
-	require("./FunctionBars/Top_BoardFilters"  ),
-	require("./FunctionBars/Left_Commands"     ),
-	require("./FunctionBars/Right_Misc"        ),
+	require("./FunctionBars/Bottom_CardFilters").default,
+	require("./FunctionBars/Top_BoardFilters"  ).default,
+	require("./FunctionBars/Left_Commands"     ).default,
+	require("./FunctionBars/Right_Misc"        ).default,
 )
 
 //###  KeyBindings  ###
