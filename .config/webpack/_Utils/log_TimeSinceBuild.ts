d23624332a73536991f003c623bd0a4c3fbf7e71
Enteleform <enteleform@gmail.com>
2019-04-15 11:17:48 -0400

    ..

--- .config/webpack/_Utils/log_TimeSinceBuild.ts
+++ .config/webpack/_Utils/log_TimeSinceBuild.ts
@@ -19,7 +19,7 @@ export function get_log_TimeSinceBuild_Callback(){
 		const elapsedTime = _get_ElapsedTime(${posixTime})
 
 		const line_1  = \`│  Built  {  \${elapsedTime}  }  Ago  │\`
-		const line_2  = \`│  At     \${timeStamp}\`.padEnd((line_1.length - 1)) + "│"
+		const line_2  = \`│  At     ${timeStamp}\`.padEnd((line_1.length - 1)) + "│"
 		const divider = "".padStart((line_1.length - 2), "─")
 
 		console.log(""
