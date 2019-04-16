83bdabd6836fce85bc8cf54ba5d0dad578b7f8e4
Enteleform <enteleform@gmail.com>
2019-04-15 23:20:38 -0400

    ..

--- __src__/__Main__/__Main__.ts
+++ __src__/__Main__/__Main__.ts
@@ -1,20 +1,20 @@
 
-// //####################//
-// //###  Extensions  ###//
-// //####################//
+//####################//
+//###  Extensions  ###//
+//####################//
 
-// import "~/Extensions/CheckList/TEMP"
+import "~/Extensions/CheckList/TEMP"
 
-// import {FunctionBar} from "~/Extensions/FunctionBar/__Main__"
-// FunctionBar.load(
-// 	require("./FunctionBars/Left_Commands"     ).default,
-// 	require("./FunctionBars/Right_Misc"        ).default,
-// 	require("./FunctionBars/Top_BoardFilters"  ).default,
-// 	require("./FunctionBars/Bottom_CardFilters").default,
-// )
+import {FunctionBar} from "~/Extensions/FunctionBar/__Main__"
+FunctionBar.load(
+	require("./FunctionBars/Left_Commands"     ).default,
+	require("./FunctionBars/Right_Misc"        ).default,
+	require("./FunctionBars/Top_BoardFilters"  ).default,
+	require("./FunctionBars/Bottom_CardFilters").default,
+)
 
-// //#####################//
-// //###  KeyBindings  ###//
-// //#####################//
+//#####################//
+//###  KeyBindings  ###//
+//#####################//
 
-// import "~/KeyBindings/Filter"
+import "~/KeyBindings/Filter"
