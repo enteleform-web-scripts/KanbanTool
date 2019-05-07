8f88ac58476cc38a8365fd1fac0a0df0426c1155
Enteleform <enteleform@gmail.com>
2019-05-07 11:36:55 -0400

    ..

--- EntryPoints/Extensions/FunctionBar.js
+++ EntryPoints/Extensions/FunctionBar.js
@@ -190,10 +190,10 @@
     }
 }
 
-		const elapsedTime = _get_ElapsedTime(1557243314448)
+		const elapsedTime = _get_ElapsedTime(1557243405090)
 
 		const line_1  = `│  Built  {  ${elapsedTime}  }  Ago  │`
-		const line_2  = `│  At     11:35:14 AM`.padEnd((line_1.length - 1)) + "│"
+		const line_2  = `│  At     11:36:45 AM`.padEnd((line_1.length - 1)) + "│"
 		const divider = "".padStart((line_1.length - 2), "─")
 
 		console.log(""
--- __Main__.js
+++ __Main__.js
@@ -10570,10 +10570,10 @@ exports.KanbanTool.activeBoard = exports.activeBoard;
     }
 }
 
-		const elapsedTime = _get_ElapsedTime(1557243314448)
+		const elapsedTime = _get_ElapsedTime(1557243405090)
 
 		const line_1  = `│  Built  {  ${elapsedTime}  }  Ago  │`
-		const line_2  = `│  At     11:35:14 AM`.padEnd((line_1.length - 1)) + "│"
+		const line_2  = `│  At     11:36:45 AM`.padEnd((line_1.length - 1)) + "│"
 		const divider = "".padStart((line_1.length - 2), "─")
 
 		console.log(""
@@ -11027,9 +11027,11 @@ function _inject_CSS(modulePath) {
 }
 function _inject_HTML(modulePath) {
     const url = _get_FileURL(modulePath, { folder: Settings_1.Settings.htmlFolder, extension: "html" });
+    console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    console.log(url);
     _get_FileText(url)
         .then(html => {
-        console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        console.log("-------------------------------");
         console.log(html);
         console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
     });
