8bbdf245a91fbe3be9c2c191c045737ded6259e1
Enteleform <enteleform@gmail.com>
2019-05-11 23:10:06 -0400

    ..

--- EntryPoints/Extensions/FunctionBar.js
+++ EntryPoints/Extensions/FunctionBar.js
@@ -190,10 +190,10 @@
     }
 }
 
-		const elapsedTime = _get_ElapsedTime(1557630151775)
+		const elapsedTime = _get_ElapsedTime(1557630594014)
 
 		const line_1  = `│  Built  {  ${elapsedTime}  }  Ago  │`
-		const line_2  = `│  At     11:02:31 PM`.padEnd((line_1.length - 1)) + "│"
+		const line_2  = `│  At     11:09:54 PM`.padEnd((line_1.length - 1)) + "│"
 		const divider = "".padStart((line_1.length - 2), "─")
 
 		console.log(""
