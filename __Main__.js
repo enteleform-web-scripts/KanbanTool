889f0dbd2b9dfb08760522f87d1af48c8223a59b
Enteleform <enteleform@gmail.com>
2019-05-11 23:02:42 -0400

    ..

--- __Main__.js
+++ __Main__.js
@@ -10562,10 +10562,10 @@ exports.KanbanTool.activeBoard = exports.activeBoard;
     }
 }
 
-		const elapsedTime = _get_ElapsedTime(1557629804477)
+		const elapsedTime = _get_ElapsedTime(1557630151775)
 
 		const line_1  = `│  Built  {  ${elapsedTime}  }  Ago  │`
-		const line_2  = `│  At     10:56:44 PM`.padEnd((line_1.length - 1)) + "│"
+		const line_2  = `│  At     11:02:31 PM`.padEnd((line_1.length - 1)) + "│"
 		const divider = "".padStart((line_1.length - 2), "─")
 
 		console.log(""
@@ -12038,6 +12038,7 @@ var Layout;
 (function (Layout) {
     function update_OriginalLayout() {
         setTimeout(() => {
+            __main__1.set_CSS_Variable("KanbanToolOffsets_NavHeight", `${$("nav.navbar").height()}px`);
             __main__1.set_CSS_Variable("KanbanToolOffsets_TopHeight", `${$("." + cssVariables.container + " > .top").height()}px`);
             __main__1.set_CSS_Variable("KanbanToolOffsets_LeftWidth", `${$("." + cssVariables.container + " > .center > .left").width()}px`);
             __main__1.set_CSS_Variable("KanbanToolOffsets_RightWidth", `${$("." + cssVariables.container + " > .center > .right").width()}px`);
