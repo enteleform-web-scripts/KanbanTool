6c44ca11d7aa0632a9c6c7fdf5eb9d39268f768d
Enteleform <enteleform@gmail.com>
2019-06-06 18:02:06 -0400

    ..

--- __src__/__Main__/__Main__.ts
+++ __src__/__Main__/__Main__.ts
@@ -8,9 +8,7 @@ import "~/Extensions/CheckList/TEMP"
 import {FunctionBar} from "~/Extensions/FunctionBar/__Main__"
 FunctionBar.load(
 	require("./FunctionBars/Left_Commands"     ).default,
-	require("./FunctionBars/Left_Commands"     ).default,
 	require("./FunctionBars/Right_Misc"        ).default,
-	require("./FunctionBars/Top_BoardFilters"  ).default,
 	require("./FunctionBars/Top_BoardFilters"  ).default,
 	require("./FunctionBars/Bottom_CardFilters").default,
 )
