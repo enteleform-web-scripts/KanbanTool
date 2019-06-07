f78519275c6451074e374eb37a69054f6c911f4e
Enteleform <enteleform@gmail.com>
2019-06-07 01:46:44 -0400

    ..

--- __src__/__Main__/__Main__.ts
+++ __src__/__Main__/__Main__.ts
@@ -5,13 +5,13 @@
 
 import "~/Extensions/TODO_CheckList/TEMP"
 
-import {FunctionBar} from "~/Extensions/FunctionBar/__Main__"
+import {FunctionBar             } from "~/Extensions/FunctionBar/__Main__"
 import {get_CardType_FunctionBar} from "~/Extensions/FunctionBar/DefaultBars/CardTypes/__Main__"
 FunctionBar.load(
 	require("./FunctionBars/Left_Commands"     ).default,
 	require("./FunctionBars/Right_Misc"        ).default,
 	require("./FunctionBars/Top_BoardFilters"  ).default,
-	get_CardType_FunctionBar({rowCounts: [2, 2, 3]}),
+	get_CardType_FunctionBar({rowCounts:[4, 3], stretchCells:false}),
 )
 
 //#####################//
