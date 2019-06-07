98d36cfd0befa6bc4970471ab09955a9195a22c4
Enteleform <enteleform@gmail.com>
2019-06-06 22:18:27 -0400

    ..

--- __src__/Extensions/FunctionBar/Settings.ts
+++ __src__/Extensions/FunctionBar/Settings.ts
@@ -2,7 +2,7 @@
 import {KeyBinding} from "~/Utils/KeyBinding/__Main__"
 
 
-export const css_Timeout_MS = 1000
+export const css_Timeout_MS = 500
 
 export const autoMapped_Key_Rows = KeyBinding.alphanumericKey_Rows
 
