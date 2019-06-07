154d0302f1295fd4a51e65dea05bb245cc938430
Enteleform <enteleform@gmail.com>
2019-06-07 15:12:44 -0400

    ..

--- __src__/__Main__/FunctionBars/Top_BoardFilters.ts
+++ __src__/__Main__/FunctionBars/Top_BoardFilters.ts
@@ -17,16 +17,16 @@ export default new FunctionBar({
 			new Entry({
 				name: "Today",
 				callback: () => {
-					Show.rows({include:["Routine", "Tasks.Active"]})
 					Show.allColumns()
+					Show.rows({include:["Routine", "Tasks.Active"]})
 					Hide.emptyColumns()
 				},
 			}),
 			new Entry({
 				name: "Today.All",
 				callback: () => {
-					Show.rows({include:["Routine", "Tasks.Active"]})
 					Show.allColumns()
+					Show.rows({include:["Routine", "Tasks.Active"]})
 				},
 			}),
 			new Entry({
@@ -42,24 +42,24 @@ export default new FunctionBar({
 			new Entry({
 				name: "Routine",
 				callback: () => {
-					Show.rows({include:["Routine"]})
 					Show.allColumns()
+					Show.rows({include:["Routine"]})
 					Hide.emptyColumns()
 				},
 			}),
 			new Entry({
 				name: "Tasks.Active",
 				callback: () => {
-					Show.rows({include:["Tasks.Active"]})
 					Show.allColumns()
+					Show.rows({include:["Tasks.Active"]})
 					Hide.emptyColumns()
 				},
 			}),
 			new Entry({
 				name: "Routine.Short",
 				callback: () => {
-					Show.rows({include:["Routine.Short"]})
 					Show.allColumns()
+					Show.rows({include:["Routine.Short"]})
 					Hide.emptyColumns()
 				},
 			}),
