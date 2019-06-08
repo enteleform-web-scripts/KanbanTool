4ba1c121b9da53f4ad9a3c81259f1347d1517bb0
Enteleform <enteleform@gmail.com>
2019-06-08 16:23:52 -0400

    ..

--- __src__/Extensions/FunctionBar/DefaultBars/CardTypes/CallbackManager.ts
+++ __src__/Extensions/FunctionBar/DefaultBars/CardTypes/CallbackManager.ts
@@ -41,8 +41,8 @@ export namespace CallbackManager{
 	function _get_Callbacks(cardType:CardType){
 		return {
 			on_Layout: function(cell:JQuery){
-				set_CSS_Variable(cell, "filterColor", cardType.bgColor)
-				console.log(">>>", cell)
+				set_CSS_Variable(cell, cssVariables.filterColor, cardType.bgColor)
+				console.log(">>>", cell, cssVariables.filterColor, cardType.bgColor)
 
 				const update_CSS = _get_UpdateCSS_Callback(cell, cardType)
 				CardType_Filter.on_Update(update_CSS)
