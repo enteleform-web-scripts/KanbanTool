3e158b7289661e355c0af2b163c82e80143952a5
Enteleform <enteleform@gmail.com>
2019-06-08 17:57:33 -0400

    ..

--- __src__/Utils/CSS_Variables/__main__.ts
+++ __src__/Utils/CSS_Variables/__main__.ts
@@ -6,6 +6,8 @@
 export function set_CSS_Variable(key:string,                  value:(string|number))
 export function set_CSS_Variable(element:JQuery,  key:string, value:(string|number))
 export function set_CSS_Variable(arg_1?, arg_2?, arg_3?                            ){
+	console.log(`-----------------------------`)
+	console.log(`{arg_1:'${arg_1}', arg_2:'${arg_2}', arg_3:'${arg_3}'}`)
 	const {element, key, value} = _get_Arguments(arg_1, arg_2, arg_3)
 	console.log(`{element:'${element}', key:'${key}', value:'${value}'}`)
 
