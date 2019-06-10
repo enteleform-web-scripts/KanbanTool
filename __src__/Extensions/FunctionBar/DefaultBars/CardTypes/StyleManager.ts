54d373ac44d174890bae1409c7df76fc17768ba4
Enteleform <enteleform@gmail.com>
2019-06-09 21:42:51 -0400

    ..

--- __src__/Extensions/FunctionBar/DefaultBars/CardTypes/StyleManager.ts
+++ __src__/Extensions/FunctionBar/DefaultBars/CardTypes/StyleManager.ts
@@ -15,3 +15,19 @@
 // callback()
 
 // KT.boards.on("change", callback)
+
+//###  TaskStates  ###//
+// low:     #7268FD
+// medium:  #FDE568
+// high:    #FF974B
+// urgent:  #E65868
+
+//###  Today  ###//
+// --title_BorderColor:      #7CCE9B
+// --title_BackgroundColor:  #CDEAD8
+// --title_ForegroundColor:  #636B66
+
+//###  Task  ###//
+// --title_BorderColor:      #B1B1B1
+// --title_BackgroundColor:  #CCCCCC
+// --title_ForegroundColor:  #505050
