f24000c06de30b8018ab05825842f5ee0bf9d346
Enteleform <enteleform@gmail.com>
2019-06-09 20:59:29 -0400

    ..

---
+++ __src__/Extensions/FunctionBar/DefaultBars/CardTypes/StyleManager.ts
@@ -0,0 +1,17 @@
+
+
+// const callback = ()=>{
+// 	console.log("--------------")
+// 	$.find("kt-task").forEach(element=>{
+// 		$element = $(element)
+// 		const taskID      = $element.data("task-id")
+// 		const model       = KT.tasks.load(taskID)
+// 		const cardType_ID = model.cardType().id
+// 		console.log($element, model)
+// 		$element.css({border:"3px solid #F00"})
+// 	})
+// }
+
+// callback()
+
+// KT.boards.on("change", callback)
