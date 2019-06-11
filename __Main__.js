601f5adc6c8da328459bc49bd1e416fb5e27a7ba
Enteleform <enteleform@gmail.com>
2019-06-11 11:30:46 -0400

    ..

--- __Main__.js
+++ __Main__.js
@@ -10620,10 +10620,10 @@ return jQuery;
     }
 }
 
-		const elapsedTime = _get_ElapsedTime(1560266787282)
+		const elapsedTime = _get_ElapsedTime(1560267035726)
 
 		const line_1  = `│  Built  {  ${elapsedTime}  }  Ago  │`
-		const line_2  = `│  At     11:26:27 AM`.padEnd((line_1.length - 1)) + "│"
+		const line_2  = `│  At     11:30:35 AM`.padEnd((line_1.length - 1)) + "│"
 		const divider = "".padStart((line_1.length - 2), "─")
 
 		console.log(""
