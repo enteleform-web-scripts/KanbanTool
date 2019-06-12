5698a64a30fdf8e82e9ec338b2de574f46434297
Enteleform <enteleform@gmail.com>
2019-06-12 17:15:41 -0400

    ..

--- EntryPoints/Extensions/CardType_Manager.js
+++ EntryPoints/Extensions/CardType_Manager.js
@@ -190,10 +190,10 @@
     }
 }
 
-		const elapsedTime = _get_ElapsedTime(1560373921793)
+		const elapsedTime = _get_ElapsedTime(1560374131749)
 
 		const line_1  = `│  Built  {  ${elapsedTime}  }  Ago  │`
-		const line_2  = `│  At     5:12:01 PM`.padEnd((line_1.length - 1)) + "│"
+		const line_2  = `│  At     5:15:31 PM`.padEnd((line_1.length - 1)) + "│"
 		const divider = "".padStart((line_1.length - 2), "─")
 
 		console.log(""
