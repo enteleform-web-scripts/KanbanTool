a1a675d9c4d6bdc571f9135031eca9e7cb78844c
Enteleform <enteleform@gmail.com>
2019-06-13 13:37:43 -0400

    ..

--- __src__/__Main__/Extensions/Keybindings_Default.ts
+++ __src__/__Main__/Extensions/Keybindings_Default.ts
@@ -1,6 +1,6 @@
 
 import {focus_SearchField} from "~/KeyBindings/focus_SearchField"
-focus_SearchField.initialize()
+focus_SearchField.initialize(["ctrl", "shift", "alt", "t"])
 
 import {show_FilterMenu} from "~/KeyBindings/show_FilterMenu"
 show_FilterMenu.initialize()
