a810b53385fa3438960f175a07050b0ea8e37658
Enteleform <enteleform@gmail.com>
2019-06-13 13:48:09 -0400

    implemented basic card KeyBindings

--- __src__/__Main__/Extensions/Keybindings_Default.ts
+++ __src__/__Main__/Extensions/Keybindings_Default.ts
@@ -1,9 +1,5 @@
 
-import {focus_SearchField} from "~/KeyBindings/focus_SearchField"
-focus_SearchField.initialize()
+import {archive_Cards    } from "~/KeyBindings/archive_Cards";      archive_Cards    .initialize();
+import {focus_SearchField} from "~/KeyBindings/focus_SearchField";  focus_SearchField.initialize();
+import {move_Cards       } from "~/KeyBindings/move_Cards";         move_Cards       .initialize();
 
-import {move_Cards} from "~/KeyBindings/move_Cards"
-move_Cards.initialize()
-
-import {archive_Cards} from "~/KeyBindings/archive_Cards"
-archive_Cards.initialize()
