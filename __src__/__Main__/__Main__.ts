e84aeddac17d57da5174a55c933d828faf59cb22
Enteleform <enteleform@gmail.com>
2019-06-13 09:19:56 -0400

    ..

--- __src__/__Main__/__Main__.ts
+++ __src__/__Main__/__Main__.ts
@@ -2,6 +2,7 @@
 import "./Extensions/FunctionBar_Bottom_CardTypeManager"
 import "./Extensions/FunctionBar_Left_Commands"
 import "./Extensions/FunctionBar_Top_BoardFilters"
+import "./Extensions/KeyBindings"
 
 //###  Extensions - TEMP  ###//
 import "~/Extensions/TODO_CheckList/TEMP"
