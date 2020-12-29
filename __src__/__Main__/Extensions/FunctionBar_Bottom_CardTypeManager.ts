ac91f825d29fa70dabc7b40f09eff60511a651e1
Enteleform <enteleform@gmail.com>
2020-12-29 18:09:40 -0500

    ..

--- __src__/__Main__/Extensions/FunctionBar_Bottom_CardTypeManager.ts
+++ __src__/__Main__/Extensions/FunctionBar_Bottom_CardTypeManager.ts
@@ -34,45 +34,45 @@ const taskSettings = {
 //###  FunctionBar  ###//
 //#####################//
 
-CardType_Manager.initialize_Manual({
+CardType_Manager.initialize_Auto({
 	cellWidth: 75,
 
 	functionBar_Options:{
-		singleContainer: true,
+		//singleContainer: true,
 		stretchCells:    false,
 	},
 
-	cardTypes: [
-		{"Task":[
-			{/*###  Task_Low    ###*/ name:"Low",    borderAccent_Color:priorityColors.low,    ...taskSettings},
-			{/*###  Task_Medium ###*/ name:"Medium", borderAccent_Color:priorityColors.medium, ...taskSettings},
-			{/*###  Task_High   ###*/ name:"High",   borderAccent_Color:priorityColors.high,   ...taskSettings},
-			{/*###  Task_Urgent ###*/ name:"Urgent", borderAccent_Color:priorityColors.urgent, ...taskSettings},
-		]},
-		{"Today":[
-			{/*###  Today_Low    ###*/ name:"Low",    borderAccent_Color:priorityColors.low,    ...todaySettings},
-			{/*###  Today_Medium ###*/ name:"Medium", borderAccent_Color:priorityColors.medium, ...todaySettings},
-			{/*###  Today_High   ###*/ name:"High",   borderAccent_Color:priorityColors.high,   ...todaySettings},
-			{/*###  Today_Urgent ###*/ name:"Urgent", borderAccent_Color:priorityColors.urgent, ...todaySettings},
-		]},
-		{"Misc":[
-			{ //###  Task_Daily  ###//
-				name:                "Daily",
-				background_Color:    "hsl(215, 80%, 85%)",
-				foreground_Color:    "hsl(215, 20%, 40%)",
-				borderColor_Inside:  "hsl(215, 40%, 50%)",
-				borderColor_Main:    "hsl(215, 40%, 65%)",
-				borderColor_Outside: "hsl(215, 50%, 50%)",
-			},
-			{ //###  Book  ###//
-				name:                "Book",
-				background_Color:    "hsl(255, 70%, 85%)",
-				foreground_Color:    "hsl(255, 20%, 40%)",
-				borderColor_Inside:  "hsl(255, 40%, 50%)",
-				borderColor_Main:    "hsl(255, 40%, 65%)",
-				borderColor_Outside: "hsl(255, 50%, 55%)",
-			},
-		]},
-	],
+	//cardTypes: [
+	//	{"Task":[
+	//		{/*###  Task_Low    ###*/ name:"Low",    borderAccent_Color:priorityColors.low,    ...taskSettings},
+	//		{/*###  Task_Medium ###*/ name:"Medium", borderAccent_Color:priorityColors.medium, ...taskSettings},
+	//		{/*###  Task_High   ###*/ name:"High",   borderAccent_Color:priorityColors.high,   ...taskSettings},
+	//		{/*###  Task_Urgent ###*/ name:"Urgent", borderAccent_Color:priorityColors.urgent, ...taskSettings},
+	//	]},
+	//	{"Today":[
+	//		{/*###  Today_Low    ###*/ name:"Low",    borderAccent_Color:priorityColors.low,    ...todaySettings},
+	//		{/*###  Today_Medium ###*/ name:"Medium", borderAccent_Color:priorityColors.medium, ...todaySettings},
+	//		{/*###  Today_High   ###*/ name:"High",   borderAccent_Color:priorityColors.high,   ...todaySettings},
+	//		{/*###  Today_Urgent ###*/ name:"Urgent", borderAccent_Color:priorityColors.urgent, ...todaySettings},
+	//	]},
+	//	{"Misc":[
+	//		{ //###  Task_Daily  ###//
+	//			name:                "Daily",
+	//			background_Color:    "hsl(215, 80%, 85%)",
+	//			foreground_Color:    "hsl(215, 20%, 40%)",
+	//			borderColor_Inside:  "hsl(215, 40%, 50%)",
+	//			borderColor_Main:    "hsl(215, 40%, 65%)",
+	//			borderColor_Outside: "hsl(215, 50%, 50%)",
+	//		},
+	//		{ //###  Book  ###//
+	//			name:                "Book",
+	//			background_Color:    "hsl(255, 70%, 85%)",
+	//			foreground_Color:    "hsl(255, 20%, 40%)",
+	//			borderColor_Inside:  "hsl(255, 40%, 50%)",
+	//			borderColor_Main:    "hsl(255, 40%, 65%)",
+	//			borderColor_Outside: "hsl(255, 50%, 55%)",
+	//		},
+	//	]},
+	//],
 })
 
